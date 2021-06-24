d0e77c4483515e31af4165f0fe13dff43672e25f
Geet Choubey <geetchoubey@gmail.com>
2021-06-24 16:02:31 -0400

    Added styling to header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './index.css';
 
 const HeaderComponent = () => {
     return (
         <header>
-            Hello Test
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="navbar-header">
+                    <h1 class="navbar-brand mb-0 h1">Whe-Whe Lotto System</h1>
+                </div>
+
+            </nav>
         </header>
     )
 }
